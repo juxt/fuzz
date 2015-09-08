83f40f79ad6b68acac5af4a8e03364e62bb9ba70
Jon Pither <jon.pither@gmail.com>
2015-09-08 09:21:37 +0100

    Welcome html

--- src/cljs/fuzz/welcome.cljs
+++ src/cljs/fuzz/welcome.cljs
@@ -1,6 +1,7 @@
 (ns fuzz.welcome)
 
 (defn handler [target params]
+  (set! (.-innerHTML target) "<div class=\"juxt-div\"><p>Hello, let's get rocking</p></div>")
 
   ;; I don't do anything useful :-)
   (println "welcome"))
