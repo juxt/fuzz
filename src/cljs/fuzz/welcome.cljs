16dd398c0a30c62878e850b8ca05b898e44a010a
Jon Pither <jon.pither@gmail.com>
2015-09-08 09:23:08 +0100

    remove comment

--- src/cljs/fuzz/welcome.cljs
+++ src/cljs/fuzz/welcome.cljs
@@ -3,5 +3,4 @@
 (defn handler [target params]
   (set! (.-innerHTML target) "<div class=\"juxt-div\"><p>Hello, let's get rocking</p></div>")
 
-  ;; I don't do anything useful :-)
   (println "welcome"))
